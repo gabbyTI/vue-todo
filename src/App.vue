195b54cc91717e81f7e93f1331aed7bcad66c68f
Gabriel Ibenye <gabrielibenye@gmail.com>
2023-08-22 05:44:28 +0100

    Section-5: Navigation Component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
-<script setup></script>
+<script setup>
+import TodoHeader from './components/TodoHeader.vue';
+</script>
 
 <template>
+  <TodoHeader />
   <RouterView />
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;700&display=swap');
+
+* {
+  font-family: 'Rubik', sans-serif;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 1100px;
+  margin: 0 auto;
+}
+</style>
